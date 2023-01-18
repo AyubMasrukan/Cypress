3a51372debe9b8db78a85a9b0be4a6cf14611b1c
Ayub Masrukan <masrukanjunior@gmail.com>
2023-01-18 23:16:52 +0700

    first commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,32 @@
+describe('Login', () => {
+
+    it('input valid username dan password then Login', () => {
+
+      cy.visit('https://the-internet.herokuapp.com/login')
+
+      cy.get('#username').type("tomsmith")
+
+      cy.get('#password').type("SuperSecretPassword!")
+
+      cy.get('.radius').click()
+    })
+
+  })
+
+
+  describe('Login', () => {
+
+    it('input invalid username dan password then Login', () => {
+
+      cy.visit('https://the-internet.herokuapp.com/login')
+
+      cy.get('#username').type("Tomsmith")
+
+      cy.get('#password').type("SuperSecretPassword!")
+
+      cy.get('.radius').click()
+
+      cy.get('#flash')
+    })
+
+  })
